01f0abfa0fe172d1b0c180db421e0b8efc98ef03
Joao Francisco <jfpmelo@gmail.com>
2025-05-15 17:03:54 -0300

    backend user

---
+++ src/routes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express'
+import { CreateUserController } from './controllers/user/CreateUserController'
+import { AuthUserController } from './controllers/user/AuthUserController'
+import { isAuthenticated } from './middlewares/isAuthenticated'
+import { DetailuserController } from './controllers/user/DetailUserController'
+
+const router = Router()
+
+// Users
+router.post('/users', new CreateUserController().handle)
+router.post('/session', new AuthUserController().handle)
+router.get('/me', isAuthenticated,  new DetailuserController().handle )
+
+export { router }
---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import express, { Request, Response, NextFunction } from "express";
+import 'express-async-errors'
+import cors from 'cors'
+import dotenv from 'dotenv';
+
+import { router } from "./routes";
+
+dotenv.config(); // para usar as constantes do .env
+
+const app = express()
+app.use(express.json()) // define que o servidor vai retornar json
+app.use(cors()) // habilita qualquer ip poder fazer requisiçao
+app.use(router) // define as rotas que estao no routes.ts
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction)=>{
+    if(err instanceof Error){
+        return res.status(400).json({
+            error: err.message
+        })
+    }
+
+    return res.status(500).json({
+        status: 'error',
+        message: 'Internal server error.'
+    })
+})
+
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, () => {
+    console.log('Servidor Online - Porta ' + PORT);
+});

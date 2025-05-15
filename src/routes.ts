831bfb719763f42d3cde3db1d8aa9e0de65da1d9
Joao Francisco <jfpmelo@gmail.com>
2025-05-15 19:05:02 -0300

    finish category, product and orders

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,18 @@ import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
 import { isAuthenticated } from './middlewares/isAuthenticated'
 import { DetailuserController } from './controllers/user/DetailUserController'
+import { CreateCategoryController } from './controllers/category/CreateCategoryController'
+import { ListCategoryController } from './controllers/category/ListCategoryController'
+import { CreateProductController } from './controllers/product/CreateProductController'
+import { ListByCategoryController } from './controllers/product/ListByCategoryController'
+import { CreateOrderController } from './controllers/order/CreateOrderController'
+import { RemoveOrderController } from './controllers/order/RemoveOrderController'
+import { AddItemController } from './controllers/order/AddItemController'
+import { RemoveItemController } from './controllers/order/RemoveItemController'
+import { SendOrderController } from './controllers/order/SendOrderController'
+import { ListOrdersController } from './controllers/order/ListOrdersController'
+import { DetailOrderController } from './controllers/order/DetailOrderController'
+import { FinishOrderController } from './controllers/order/FinishOrderController'
 
 const router = Router()
 
@@ -10,5 +22,26 @@ const router = Router()
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 router.get('/me', isAuthenticated,  new DetailuserController().handle )
+
+// Categories
+router.post('/category', isAuthenticated, new CreateCategoryController().handle )
+router.get('/category', isAuthenticated, new ListCategoryController().handle )
+
+// Products
+router.post('/product', isAuthenticated, new CreateProductController().handle )
+router.get('/category/product', isAuthenticated, new ListByCategoryController().handle )
+
+// Orders
+router.post('/order', isAuthenticated, new CreateOrderController().handle )
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle )
+router.post('/order/add', isAuthenticated, new AddItemController().handle )
+router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle )
+router.put('/order/send', isAuthenticated, new SendOrderController().handle )
+router.get('/orders', isAuthenticated, new ListOrdersController().handle )
+router.get('/order/detail', isAuthenticated, new DetailOrderController().handle )
+router.put('/order/finish', isAuthenticated, new FinishOrderController().handle )
+
+
+
 
 export { router }
--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,16 @@ import cors from 'cors'
 import dotenv from 'dotenv';
 
 import { router } from "./routes";
+import fileUpload = require("express-fileupload");
 
 dotenv.config(); // para usar as constantes do .env
 
 const app = express()
 app.use(express.json()) // define que o servidor vai retornar json
 app.use(cors()) // habilita qualquer ip poder fazer requisiçao
+app.use(fileUpload({
+    limits: { fileSize: 10 * 1024 * 1024 }
+}))
 app.use(router) // define as rotas que estao no routes.ts
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction)=>{
